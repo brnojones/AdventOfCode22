93928465962191102c54b5dcd4ebc12cd67c2d71
Rowan Jones <brnojones@hotmail.com>
2022-12-10 22:28:04 +1100

    D3P1

--- AdventOfCode/Days/Three.cpp
+++ AdventOfCode/Days/Three.cpp
@@ -19,17 +19,67 @@ int Three::PartOne()
 #ifdef SPEW
 		cout << text;
 #endif
+		int len = text.length();		
+		char dupe = 0;
 
+		for (int i = 0; i < len / 2; i++)
+		{
+			for (int j = len / 2; j < len; j++)
+			{
+				if (text[i] == text[j])
+				{
+					dupe = text[i];
+					break;
+				}
+			}
+		}
 
+		int score = ScoreFromChar(dupe);
+		runningTotal += score;
 
 #ifdef SPEW
+		cout << "Dupe = " << dupe << " - Score = " << score;
 		cout << "\n";
 #endif
 	}
-	return 0;
+	return runningTotal;
 }
 
 int Three::PartTwo()
 {
-	return 0;
+	string text;
+	ifstream file(filename);
+
+	int runningTotal = 0;
+	int groupCounter = 0;
+
+	string texts[3];
+
+	while (getline(file, text))
+	{
+#ifdef SPEW
+		cout << text;
+#endif
+		int len = text.length();
+		texts[groupCounter] = text;
+
+		if (groupCounter >= 2)
+		{
+			groupCounter = 0;
+		}
+		groupCounter++;
+#ifdef SPEW
+		cout << "\n";
+#endif
+	}
+	return runningTotal;
+}
+
+int Three::ScoreFromChar(char c)
+{
+	if (c >= 97)
+	{
+		return c - 96;
+	}
+	return c - 38;
 }
